73bc93d941bc0b90c5ef00fe96aabebfe4881db5
Chuck Yin <chuckyin@oculus.com>
2019-06-20 11:07:49 -0700

    start from zero, sorry, memory loss after 9 month idle, prefer scratch

--- tsc/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use tsc in a project::
-
-    import tsc
--- tsc/tsc/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for tsc."""
-
-__author__ = """Chuck Yin"""
-__email__ = 'chuckyeyin@gmail.com'
-__version__ = '0.1.0'
